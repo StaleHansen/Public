4a8d118ed2a4963a05d9d4f81b2b7509fb944009
Ståle Hansen [MVP] <stale@msunified.net>
2020-07-16 11:21:53 +0200

    Update FlowCodeExample.ps1

--- Start-SimplePomodoro/FlowCodeExample.ps1
+++ Start-SimplePomodoro/FlowCodeExample.ps1
@@ -1,1 +1,14 @@
-﻿
+﻿[int]$Minutes = 25 #Duration of your Pomodoro Session, default is 25 minutes
+[string]$Secret = "MySecret" #Secret for the flow trigger
+[string]$AutomateURI = "YourFlowTriggerURI" #The URI used in the webrequest to your flow
+
+
+#Invoking PowerAutomate to change set current time on your Focus time calendar event, default length is 25 minutes
+    $body = @()
+    $body = @"
+        { 
+            "Duration":$Minutes,
+            "Secret":"$Secret"
+        }
+"@
+Invoke-RestMethod -Method Post -Body $Body -Uri $AutomateURI -ContentType "application/json"
